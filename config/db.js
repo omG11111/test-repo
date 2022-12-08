bc27c59e307ca4e32a9c68a11af69d6425434991
omG11111 <omgumber01@gmail.com>
2022-12-08 16:48:24 +0530

    second hit

---
+++ config/db.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const mongoose=require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+    console.log("connected success");
+}).catch(err=>{
+    console.log("connection fail",err);
+})
