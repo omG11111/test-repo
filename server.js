6f5fab485a5a2f9bfc8e838435f460c60d96f5e4
omG11111 <omgumber01@gmail.com>
2022-12-08 16:38:49 +0530

    first test push

---
+++ server.js
@@ -0,0 +1,33 @@
+require("./config/db")
+const app=require("express")();
+const bodyParser=require("express").json;
+const port = process.env.PORT || 3000;
+const User=require("./models/User")
+const cors=require("cors");
+
+
+app.use(bodyParser())
+app.use(cors());
+app.post("/any",(req,res)=>{
+    console.log("hitterdddd seccess",req);
+    const user=new User({
+        name:"SAdsada",
+        email:"om@gmail.com"
+    })
+    user.save().then((data)=>{
+        res.json({
+            message :"Seccess fully addd",
+            data:data
+        })
+        
+    }).catch(err=>{
+        res.json({
+            message:"fail",
+            error:err
+        })
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`alive at ${port}`);
+})
