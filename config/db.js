ddee6549ec0b256e387d36889d372093b8543ea6
omG11111 <omgumber01@gmail.com>
2022-12-08 21:53:57 +0530

    dfdfff

--- config/db.js
+++ config/db.js
@@ -4,8 +4,13 @@ const mongoose=require("mongoose");
 mongoose.set('strictQuery', false);
 
 mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
-    console.log("connected success");
+    console.log("connected successddddddddddsdffsdsfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsd sdfsdf");
+    
+    
+    
+    
+   
 }).catch(err=>{
-    console.log("connection fail",err);
+    console.log("connection fail");
 })
 //asdasdaasdasd
