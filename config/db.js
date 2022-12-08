c08ee58c31c1fc29a1a92c25934d0649b0f1ede3
omG11111 <omgumber01@gmail.com>
2022-12-08 22:01:14 +0530

    sds

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mongoose=require("mongoose");
 mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log("connected successddddddddddsdffsdsfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsd sdfsdf");
     
     
